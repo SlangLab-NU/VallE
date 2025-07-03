52d0d0efc5fa50060b3c3588f7f6754afcb498be
jordan-git-lewis <jlgitrepo@gmail.com>
2025-07-03 14:11:05 -0400

    updated interactive shell script

--- run_interactive_shell.sh
+++ run_interactive_shell.sh
@@ -1,6 +1,9 @@
+module load singularity/3.10.3
+
 export VALLE_ROOT=/scratch/lewis.jor
 export VALLE_REPO_ROOT=$VALLE_ROOT/VallE
 export SINGULARITYENV_PYTHONPATH="$VALLE_REPO_ROOT:/workspace/icefall:$PYTHONPATH"
 export singularity_image=$VALLE_REPO_ROOT/concat_speakers_on_dev_set.sif
 
-singularity shell --nv --bind $VALLE_REPO_ROOT:$VALLE_REPO_ROOT $singularity_image
+singularity shell --nv --bind $VALLE_REPO_ROOT:$VALLE_REPO_ROOT \
+                        --bind /work/van-speech-nlp/UASpeech:/scratch/lewis.jor/UASpeech $singularity_image
