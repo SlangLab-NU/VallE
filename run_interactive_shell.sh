0d5b82363a1a1593cbaff12f732dacc0fe16310b
jordan-git-lewis <jlgitrepo@gmail.com>
2026-04-15 12:20:41 -0400

    update git ignore and bash script

--- run_interactive_shell.sh
+++ run_interactive_shell.sh
@@ -6,6 +6,21 @@ export VALLE_REPO_ROOT=$VALLE_ROOT/VallE
 export APPTAINERENV_PYTHONPATH="$VALLE_REPO_ROOT:/workspace/icefall:$PYTHONPATH"
 export apptainer_image=$VALLE_REPO_ROOT/valle_container2.sif
 
+# Point all cache dirs to writable scratch space
+export APPTAINERENV_NUMBA_CACHE_DIR=$VALLE_ROOT/cache/numba
+export APPTAINERENV_TRANSFORMERS_CACHE=$VALLE_ROOT/cache/huggingface
+export APPTAINERENV_HF_HOME=$VALLE_ROOT/cache/huggingface
+export APPTAINERENV_LHOTSE_CACHE_DIR=$VALLE_ROOT/cache/lhotse
+export APPTAINERENV_TORCH_HOME=$VALLE_ROOT/cache/torch
+export APPTAINERENV_MPLCONFIGDIR=$VALLE_ROOT/cache/matplotlib
+
+mkdir -p $VALLE_ROOT/cache/numba
+mkdir -p $VALLE_ROOT/cache/huggingface
+mkdir -p $VALLE_ROOT/cache/lhotse
+mkdir -p $VALLE_ROOT/cache/torch
+mkdir -p $VALLE_ROOT/cache/matplotlib
+
 apptainer shell --nv \
-        --bind /scratch/lewis.jor:/scratch/lewis.jor \
-       	$apptainer_image
+    --no-home \
+    --bind /scratch/lewis.jor:/scratch/lewis.jor \
+    $apptainer_image
